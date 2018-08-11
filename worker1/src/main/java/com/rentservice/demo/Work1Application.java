46fc5dd9b28c850b4beb8bd3cf8beed9cd4de2bd
dj458 <fzheng8@vt.edu>
2018-08-11 13:51:57 -0400

    add worker1 file

---
+++ worker1/src/main/java/com/rentservice/demo/Work1Application.java
@@ -0,0 +1,12 @@
+package com.rentservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Work1Application.class, args);
+    }
+}
