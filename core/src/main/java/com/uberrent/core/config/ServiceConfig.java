a5b576fea95b10bee8411d68421195971224418d
dj458 <fzheng8@vt.edu>
2018-09-06 21:14:49 -0400

    spring boot started

--- core/src/main/java/com/uberrent/core/config/ServiceConfig.java
+++ core/src/main/java/com/uberrent/core/config/ServiceConfig.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 import java.io.IOException;
-
 
 @Configuration
 @ComponentScan(basePackages = "com.uberrent.core")
--- worker1/src/main/java/com/rentservice/demo/Config/AppCofig.java
+++ worker1/src/main/java/com/rentservice/demo/Config/AppCofig.java
@@ -1,11 +1,35 @@
 package com.rentservice.demo.Config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import com.uberrent.core.config.DataBaseConfig;
+import com.uberrent.core.config.ServiceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.env.Environment;
 
 @Configuration
-@ComponentScan(basePackages = "com.uberrent.core",excludeFilters = @ComponentScan.Filter(type=FilterType.REGEX,pattern={"JmsConfig","JmsConfig"}))
+@Import(ServiceConfig.class)
+@ComponentScan(basePackages = "com.uberrent.core",excludeFilters = @ComponentScan.Filter(type=FilterType.REGEX,pattern={"JmsConfig"}))
 public class AppCofig {
+    @Autowired
+    private Environment env;
+
+    @Bean(name = "appProperties")
+    public PropertiesFactoryBean getDatabaseConfig() throws Exception {
+        String profile = env.getActiveProfiles()[0];
+        PropertiesFactoryBean factory = new PropertiesFactoryBean();
+        factory.setLocation(new ClassPathResource("META-INF/env/" + profile + "-app.properties"));
+        return factory;
+    }
+
+    @Bean(name = "shareProperties")
+    public PropertiesFactoryBean getShareProperties() throws Exception {
+        PropertiesFactoryBean factory = new PropertiesFactoryBean();
+        factory.setLocation(new ClassPathResource("META-INF/share.properties"));
+        return factory;
+    }
+
+
 
 }
